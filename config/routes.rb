2735106e3c555fede7b6d77f3446854bfa0a6fe6
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-09-09 10:13:52 +0300

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/login", to: "session#create"
+  delete "/logout", to: "session#destroy"
+
+  # user routes
+  get "me", to: "users#show"
 end
